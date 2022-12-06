626ac4dc523006743943ea023f2897e142019742
nickolay7 <shkelal50@gmail.com>
2022-12-06 10:35:14 +0300

    refactored page 404

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -4,10 +4,10 @@ import { Container } from 'layout/Container'
 
 export const NotFoundPage = () => (
   <Container>
-    <h1 className='ui-title-1 mb-4'>
+    <h1 className='ui-title-1 mb-4 text-center'>
       404 - NotFound
     </h1>
-    <p>
+    <p className='text-center'>
       Page not found. Visit <Link className='ui-link' to='/'>Home page</Link>.
     </p>
   </Container>
